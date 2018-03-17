5929f864e5d80799f12eb9ee9f7210d564aed1a3
Steve Valasek <33697089+steve626@users.noreply.github.com>
2018-03-17 14:06:30 -0700

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -72,3 +72,70 @@ function decreaseValue() {
 
   //Google Maps API
   // var APIkey ="AIzaSyCB_NNmYoFVRc6qz7_vp6IJ-9-GIkEvy5U";
+
+// Is It Worth It
+//compare driving vs flying to get from point A to point B
+
+//globals
+var gasPrice = 2.536 //$-gallon from aaa.com
+var hourValue = 25 //$-hour based on median income in usa
+
+var mpgObj = {
+    "electric":33, 
+    "compact": 60,
+    "midSize": 44,
+    "fullSize":37,
+    "smallSuv": 34,
+    "largSuv": 29,
+    "miniVan": 31,
+    "pickUp": 26
+};
+// var electricMpg = 83
+// var compactMpg = 60
+// var midSizeMpg = 44
+// var fullSizeMpg = 37
+// var smallSuvMpg = 34
+// var largeSuvMpg = 29
+// var miniVanMpg = 31
+// var pickUpMpg = 26
+
+var carCarbon = 19.6 //lbs/gallon
+var flyCarbon = 0.25 //lbs/flight mile
+
+//electric time per mile to charge = 0.43 minutes/mile traveled 
+//electric cost per mile to charge = 0.04 dollars/mile traveled
+
+
+
+//get start and end point from user input
+
+
+
+//get car type from user input
+
+//get miles and travel time from travel maps api, driving and flying
+
+//get flight cost from travel api 
+
+//if gas car picked: 
+//((tripMiles / MPG) * gasPrice) = trip cost
+
+//(tripTime * hourValue) = time cost
+
+
+
+
+//if electric car picked:
+    //miles to drive * 0.43 = time to charge, add to trip time (very generic, Tesla is much faster)
+
+    //((tripTime + chargeTime)*hourValue) = time cost
+
+    //miles to drive * 0.04 = cost of electricity for trip (very generic).
+
+    //(tripMiles * 0.04) = trip cost
+
+
+//for flight:
+    // trip cost = flight cost
+    //time cost = (flight time + 210 minutes of incidental travel time ) * hourValue
+
