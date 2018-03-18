6e40aebdc2ced0a0f93aa51d3ca9b97ea158b141
Eva Hernandez <Eva@Dylans-MacBook-Pro.local>
2018-03-17 21:38:43 -0700

    more design

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,21 @@
 
+
+//some functions for passangers information 
+function increaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number').value = value;
+}
+
 //Google Directions API
 var APIkey = "AIzaSyB1BAEdGTc2ICoqQdaJf9Rpf3p_zCZPIGg"
 var origin = "Phoenix"
@@ -13,3 +30,4 @@ var queryURL = "https://maps.googleapis.com/maps/api/directions/json?key="+APIke
       console.log(response);
       console.log(response)
     });
+
