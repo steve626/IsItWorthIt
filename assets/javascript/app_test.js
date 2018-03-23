60d305eb43862b4b0ab85806f04802869d4d2534
Steve Valasek <svalasek@gmail.com>
2018-03-22 21:43:47 -0700

    maths works

--- assets/javascript/app_test.js
+++ assets/javascript/app_test.js
@@ -91,10 +91,12 @@ if (passengers === 0) {
        console.log("lng1: "+ response.route.locations[0].latLng.lng);
        console.log("lat2: " + response.route.locations[1].latLng.lat);
        console.log("lng2: " + response.route.locations[1].latLng.lng);
+
        carCost(response);
        carEco(response);
+       
        distance(response);
-      
+
        var lat1 = parseInt(response.route.locations[0].latLng.lat);
        var lng1 = parseInt(response.route.locations[0].latLng.lng);
        var lat2 = parseInt(response.route.locations[1].latLng.lat);
@@ -113,11 +115,12 @@ if (passengers === 0) {
           return R * 2 * Math.asin(Math.sqrt(a));
         };
 
-       
+        //console.log("dist: " + R);
         console.log("dist:" +  distance(lat1, lng1, lat2, lng2));
 
         var dist =  distance(lat1, lng1, lat2, lng2);
         console.log(dist);
+       
 	     
        $("#distanceInfo").append("From " + origin + " to " + destination + " the distance is " + Math.round(response.route.distance) + " miles ");
        $("#drivetime").append("Currently, drive time is: " + response.route.formattedTime);
@@ -136,10 +139,10 @@ if (passengers === 0) {
           console.log(mpgObj[carType]);
           console.log(gasPrice);
           console.log(passengers[0].valueAsNumber);
-   			  gasCost = ((x.route.distance/mpgObj[carType])*gasPrice)/passengers[0].valueAsNumber;
+   			  gasCost = Math.round((x.route.distance/mpgObj[carType])*gasPrice)/passengers[0].valueAsNumber;
         //  return gasCost;
           console.log("GasCost: $" +gasCost);
-	  $("#carGasCost").append("Your gas cost:" + " $" + Math.round(gasCost) + " MPG");
+	  $("#carGasCost").append("Your gas cost:" + " $" + Math.round(gasCost));
   				}
         timeCost = ((parseInt(response.route.formattedTime))*25);
         console.log("timeCost: $" + timeCost);
@@ -155,63 +158,63 @@ if (passengers === 0) {
           console.log("carEco: " + driveCarbon);
 
           $("#driveEco").append(driveCarbon + "lbs CO2/person");
-          $("#drivecost").append("The total cost to drive: $" + carCost); 
         };
-					
-       		flyCost(response);
-       		flyCarbon(response);
-       		flyHours(response);
-					
-        	var flyCost = 0;
-          var flyTime = 0;
-          var flyEco = 0;
 
-  
-        function flyCost(x){
-          
+
+        $("#drivecost").append("The total cost to drive: $" + carCost);
+
+        flyCost(response);
+        flyCarbon(response);
+        //flyHours(response);
+
+    
+        var flyCost = 0;
+        var flyTime = 0;
+        var flyEco = 0;
+
+        function flyCost(x){       
         
-          console.log(x.route.distance);
-   			 flyCost =((parseInt(x.route.distance) * 0.11) + 50);
+         
+   			 flyCost =Math.round((parseInt(dist) * 0.11) + 50);
          
           console.log("Avg Fare: $"+ flyCost);
 
           $("#flycost").append("Avg airfare: $" + flyCost);
-          
-        }
         
           //need flying time
           //formula is flight duration plus 195 minutes (time driving to/from airport, waiting to board and waiting for luggage)
-        
-        
-        flyHours();
+      
 
+        };
         
+        
+
+        flyTime = (((dist/475) * 60 ) + 195 );
+        flyHours(flyTime);
+
+        console.log("fly time1: " + flyTime);
         
         function flyHours(x) {         
-            flyTime = (((dist/475) * 60 ) + 195 );
-            console.log("fly time1: " + flyTime);
-            var h = flyTime / 60 | 0;
-            var m = flyTime % 60 | 0;
-           var formattedTime = moment.utc().hours(h).minutes(m).format("HH:mm");
 
-            console.log("fly time2: " + formattedTime);
+            var h = x / 60 | 0;
+            var m = x % 60 | 0;
+            return moment.utc().hours(h).minutes(m).format("HH:mm A");
 
-            $("#flytime").append(formattedTime + "est. travel time");
+            console.log("fly time2: " + flyHours);
+
+            $("#flytime").append(flyHours(flytime) + "est. travel time");
 
         };
 
-        
-       
-
         function flyCarbon(x) {
-          var flyCo2 = (parseInt(x.route.distance)*0.25);
+          var flyCo2 = Math.round((dist) * 0.25);
         
           console.log("flyCarbon: " + flyCo2);
 
           $("#flyEco").append(flyCo2 + "lbs/CO2 per person");
         };
 
-      
+
       });
        
 
@@ -227,135 +230,11 @@ if (passengers === 0) {
    //     cache: false,
        }).then(function(response) {
          console.log(response);
-  
-         $("#transit-time").append("Transit Time: " + response.routes[0].legs[0].duration.text);
          console.log("GOOGLE TRANSIT DURATION: " + response.routes[0].legs[0].duration.text);
-         
          console.log("GOOGLE TRANSIT DISTANCE: " + response.routes[0].legs[0].distance.text);
-         var tDistance = parseInt(response.routes[0].legs[0].distance.text);
-         console.log(tDistance);
-
-         var tc = tDistance * 19 /100 | 100;
-          console.log("TC:"+tc);
-          $("#transit-cost").append("Estimated Transit Cost: $"+tc);
-          
-          var tCarb = (tDistance / 5000 * 2204.6)/4 | 100;
-          $("#transit-Eco").append("Estimated Carbon Footprint: "+tCarb+" lbs CO2/person");
  
       });
-  
-  
-  
-// function transitCost(transitCost) {
-//             var h = flyTime / 60 | 0;
-//             var m = flyTime % 60 | 0;
-//             return moment.utc().hours(h).minutes(m).format("HH:mm A");
 
-//             console.log("fly time: " + flyHours());
-
-//             $("#flytime").append(flyHours + "est. travel time");
-
-//         };
-
-       
-///////////////////////////////
-
-    //Flight API (Still Pending)
-
-  //   var queryURLF = "https://api.flightstats.com/flex/schedules/rest/v1/jsonp/from/PHX/to/LAX/departing/2018/3/30?appId=c5590eb9&appKey=+df8ff1453a113bf7b675d517326983ea";
-
-  //   $.ajax({
-  //     url: queryURLF,
-  //     method: 'GET',
-  //     dataType: 'jsonp',
-  //  //  cache: false,
-  //   }).then(function(response) {
-  //     console.log(response);
-      
-  //  });
-
-
-// 9 Result Variables
-	var carCost = 0;
-	var carTime = 0;
-	var carEco = 0;
-	var flyCost = 0;
-	var flyTime = 0;
-	var flyEco = 0;
-	var transitCost = 0;
-	var transitTime = 0;
-	var transitEco = 0;
-  
-//Average Fare = $50 + (Distance * $0.11)
-
-
-
-//electric time per mile to charge = 0.43 minutes/mile traveled 
-//electric cost per mile to charge = 0.04 dollars/mile traveled
-
-
-
-//get start and end point from user input
-
-
-
-
-//get car type from user input
-
-//get miles and travel time from travel maps api, driving and flying
-
-//get flight cost from travel api 
-
-//if gas car picked: 
-//((tripMiles / MPG) * gasPrice) = trip cost
-
-//(tripTime * hourValue) = time cost
-
-// ***functions from Steve
-//  function carTime() {
-//    if carType === "Electric" {
-//      carTime = ((driveDist * 0.43) + tripTime);
-//      return carTime;
- //   } else if carType !== "Electric" {
-//      carTime = tripTime;
- //     return carTime;
-//    }
-//  }
-  
-//  function carEco() {
-//      carEco = (for i = carType[i]; 
- //               ((driveDist/mpgObj[i])*carCarbon)/("#passengers").val().trim());
- //   return carEco;
- //   }
- // }
-    
-//    function flightTime() {
-// flightTime = ( (((time in flight)/60) + 210minutes))/60)
-//}            
-    
-//    function flyCost() {
-//  flyCost = (flightTime * hourValue) + ticketPrice;
-//}
-
-//		function flyEco() {
- //     flightDistance * flyCarbon 
- //   }   
-
-
-
-//if electric car picked:
-    //miles to drive * 0.43 = time to charge, add to trip time (very generic, Tesla is much faster)
-
-    //((tripTime + chargeTime)*hourValue) = time cost
-
-    //miles to drive * 0.04 = cost of electricity for trip (very generic).
-
-    //(tripMiles * 0.04) = trip cost
-
-
-//for flight:
-    // trip cost = flight cost
-    //time cost = (flight time + 210 minutes of incidental travel time ) * hourValue
 });
 
 
